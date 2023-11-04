0415963629a13bd2571805c01882ffbef836dcd9
Ghost <danyfox1.dm@gmail.com>
2023-11-04 11:24:55 -0300

    adding stok picker method

---
+++ stock_picker/stock_picker.rb
@@ -0,0 +1,26 @@
+def stock_picker_refact(days)
+  profit = 0
+  buy_sell_day = [0, 0]
+
+  days.each_with_index do |buy, index|
+    days[index + 1..-1].map do |sell|
+      next unless sell - buy > profit
+      profit = sell - buy
+      buy_sell_day = "buy day: #{days.index(buy) + 1} price = #{buy} \nsell day: #{days.index(sell) + 1} price = #{sell}\nprofit = #{profit}"
+    end
+  end
+  return -1 if buy_sell_day == [0, 0]
+  buy_sell_day
+end
+
+month = [17,3,6,9,15,8,6,1,10]
+puts "#{month} \n#{stock_picker_refact(month)}"
+  # => [1,4]  # for a profit of $15 - $3 == $12
+
+month = [6,4,1]
+puts "\n#{month} \n#{stock_picker_refact(month)}"
+# => []
+
+month = [17,21,4,3,6,9,5,8,15,8,6,1,10]
+puts "\n#{month} \n#{stock_picker_refact(month)}"
+# => [3, 8] # for a profit of $15 - 3 == 12
